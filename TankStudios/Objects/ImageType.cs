a5cc279a57e6d1d90699496ed04910b92d159335
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-05-29 17:55:43 -0500

    Prepared for Azure publish!  Made ol's same size as rest of text when in blog posts

--- TankStudios/Objects/ImageType.cs
+++ TankStudios/Objects/ImageType.cs
@@ -20,13 +20,13 @@ namespace TankStudios.Objects
             switch(type)
             {
                 case ImageType.Tech:
-                    path = "~/Content/freelancer/portfolio/satelliteSmall.png";
+                    path = "http://i1068.photobucket.com/albums/u458/Ryan_Tankersley/aqws_zpszpg7nkn2.png";
                     break;
                 case ImageType.Life:
-                    path = "~/Content/freelancer/portfolio/cabinSmall.png";
+                    path = "http://i1068.photobucket.com/albums/u458/Ryan_Tankersley/qwsa_zpskqgidov6.png";
                     break;
                 case ImageType.Dev:
-                    path = "~/Content/freelancer/portfolio/gpsSmall.png";
+                    path = "http://i1068.photobucket.com/albums/u458/Ryan_Tankersley/wsaq_zps93gqfqao.png";
                     break;
                 default:
                     throw new NotImplementedException(type.ToString() + " is not supported.");
